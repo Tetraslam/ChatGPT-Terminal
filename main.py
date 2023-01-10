0c096e630e4f2ce2e8294725d4d9e317d03e103e
Tetraslam <contactmoonshard@gmail.com>
2023-01-10 11:19:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import openai
+import re
+
+openai.api_key = "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"
+
+def chat(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message.strip()
+
+def format_message(message):
+    message = message.replace("\n", " ")
+    message = re.sub(" +", " ", message)
+    return message
+
+while True:
+    prompt = input("You: ")
+    response = chat(prompt)
+    print(f"Bot: {format_message(response)}")
